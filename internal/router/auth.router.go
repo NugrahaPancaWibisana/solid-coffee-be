e6b55293c73107fc644fabce944f7c4d147cba92
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-01-30 00:15:44 +0700

    feat[router]: adds auth router login feature

--- internal/router/auth.router.go
+++ internal/router/auth.router.go
@@ -1,19 +1,20 @@
 package router
 
 import (
-	// "github.com/NugrahaPancaWibisana/solid-coffee-be/internal/controller"
-	// "github.com/NugrahaPancaWibisana/solid-coffee-be/internal/repository"
-	// "github.com/NugrahaPancaWibisana/solid-coffee-be/internal/service"
+	"github.com/NugrahaPancaWibisana/solid-coffee-be/internal/controller"
+	"github.com/NugrahaPancaWibisana/solid-coffee-be/internal/repository"
+	"github.com/NugrahaPancaWibisana/solid-coffee-be/internal/service"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
 )
 
 func AuthRouter(app *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
-	// authRouter := app.Group("/auth")
+	authRouter := app.Group("/auth")
 
-	// authRepository := repository.NewAuthRepository(db)
-	// authService := service.NewAuthService(authRepository, rdb)
-	// authController := controller.NewAuthController(authService)
+	authRepository := repository.NewAuthRepository()
+	authService := service.NewAuthService(authRepository, rdb, db)
+	authController := controller.NewAuthController(authService)
 
+	authRouter.POST("/", authController.Login)
 }
