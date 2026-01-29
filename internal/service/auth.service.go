3aaf7f5ebd97f21fb4d694bf8973d7f371adff7e
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-01-30 00:12:47 +0700

    feat[service]: adds auth service login feature

--- internal/service/auth.service.go
+++ internal/service/auth.service.go
@@ -1,15 +1,82 @@
 package service
 
 import (
+	"context"
+	"log"
+	"regexp"
+
+	"github.com/NugrahaPancaWibisana/solid-coffee-be/internal/apperror"
+	"github.com/NugrahaPancaWibisana/solid-coffee-be/internal/cache"
+	"github.com/NugrahaPancaWibisana/solid-coffee-be/internal/dto"
 	"github.com/NugrahaPancaWibisana/solid-coffee-be/internal/repository"
+	"github.com/NugrahaPancaWibisana/solid-coffee-be/pkg/hash"
+	jwtutil "github.com/NugrahaPancaWibisana/solid-coffee-be/pkg/jwt"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
 )
 
 type AuthService struct {
 	authRepository *repository.AuthRepository
 	redis          *redis.Client
+	db             *pgxpool.Pool
 }
 
-func NewAuthService(authRepository *repository.AuthRepository, rdb *redis.Client) *AuthService {
-	return &AuthService{authRepository: authRepository, redis: rdb}
+func NewAuthService(authRepository *repository.AuthRepository, rdb *redis.Client, db *pgxpool.Pool) *AuthService {
+	return &AuthService{authRepository: authRepository, redis: rdb, db: db}
+}
+
+func (as *AuthService) Login(ctx context.Context, req dto.LoginRequest) (dto.User, error) {
+	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, _ := regexp.MatchString(emailRegex, req.Email)
+	if !matched {
+		return dto.User{}, apperror.ErrInvalidEmailFormat
+	}
+	
+	tx, err := as.db.Begin(ctx)
+	if err != nil {
+		log.Println(err.Error())
+		return dto.User{}, err
+	}
+	defer tx.Rollback(ctx)
+
+	data, err := as.authRepository.Login(ctx, tx, req)
+	if err != nil {
+		return dto.User{}, err
+	}
+
+	hasher := hash.Default()
+	isValid, err := hasher.Verify(req.Password, data.Password)
+	if err != nil {
+		return dto.User{}, err
+	}
+	if !isValid {
+		return dto.User{}, apperror.ErrInvalidCredential
+	}
+
+	err = as.authRepository.UpdateLastLogin(ctx, tx, data.ID)
+	if err != nil {
+		return dto.User{}, err
+	}
+
+	res := dto.User{
+		ID:        data.ID,
+		Email:     data.Email,
+		Role:      data.Role,
+		LastLogin: nil,
+	}
+
+	if data.LastLoginAt.Valid {
+		res.LastLogin = &data.LastLoginAt.Time
+	}
+
+	return res, nil
+}
+
+func (as *AuthService) GenerateJWT(ctx context.Context, user dto.User) (string, error) {
+	claims := jwtutil.NewJWTClaims(user.ID, user.Role)
+	return claims.GenToken()
+}
+
+func (as *AuthService) WhitelistToken(ctx context.Context, id int, token string) {
+	cache.SetToken(ctx, as.redis, id, token)
 }
