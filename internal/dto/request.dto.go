7d6672ab7f71026650ce59a2e082086cd4686276
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-02-01 16:41:24 +0700

    feat[dto]: adds user update profile request

--- internal/dto/request.dto.go
+++ internal/dto/request.dto.go
@@ -1,5 +1,7 @@
 package dto
 
+import "mime/multipart"
+
 type LoginRequest struct {
 	Email    string `json:"email" binding:"required,email" example:"example123@gmail.com"`
 	Password string `json:"password" binding:"required,min=8" example:"example123"`
@@ -10,4 +12,11 @@ type RegisterRequest struct {
 	Email           string `json:"email" binding:"required,email" example:"example123@gmail.com"`
 	Password        string `json:"password" binding:"required,min=8" example:"example123"`
 	ConfirmPassword string `json:"confirm_password" binding:"required,eqfield=Password" example:"example123"`
+}
+
+type UpdateProfileRequest struct {
+	Photo    *multipart.FileHeader `form:"photo"`
+	Fullname string                `form:"fullname" binding:"omitempty,min=3" example:"John Doe"`
+	Phone    string                `form:"phone" binding:"omitempty,min=3" example:"08123456789"`
+	Address  string                `form:"address" binding:"omitempty,min=3" example:"Jakarta"`
 }
