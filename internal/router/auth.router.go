5782267ec8521d49cf7ed3b9c7764724845553c1
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2026-02-12 01:10:25 +0700

    feat(router): change permission route

--- internal/router/auth.router.go
+++ internal/router/auth.router.go
@@ -19,7 +19,7 @@ func AuthRouter(app *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 
 	authRouter.POST("/", authController.Login)
 	authRouter.POST("/new", authController.Register)
-	authRouter.DELETE("/", middleware.AuthMiddleware(), middleware.RBACMiddleware("user"), authController.Logout)
+	authRouter.DELETE("/", middleware.AuthMiddleware(), middleware.RBACMiddleware("user", "admin"), authController.Logout)
 	authRouter.POST("/forgot-password", authController.ForgotPassword)
 	authRouter.POST("/forgot-password/update", authController.UpdateForgotPassword)
 }
--- internal/router/orders.router.go
+++ internal/router/orders.router.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
-)
+)	
 
 func OrderRouter(app *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	adminOrdersRouter := app.Group("/admin")
